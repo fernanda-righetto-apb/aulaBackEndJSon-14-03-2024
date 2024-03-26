04b1f5e68d7fd457f8c1dc5307fad6c9de664a66
fernanda-righetto-apb <fernanda.guaragni.estudante@petrobras.com.br>
2024-03-26 14:43:42 -0300

    lib finalizada

--- src/http-validacao.js
+++ src/http-validacao.js
@@ -1,8 +1,14 @@
+import chalk from "chalk";
+
 async function checaStatus(listaUrls){
     const arrStatus = await Promise.all(
         listaUrls.map(async (url)=>{
-            const response = await fetch(url);
-            return response.status; 
+            try{
+                const response = await fetch(url);
+                return response.status; 
+            }catch(erro){
+                return manejaErros(erro);
+            }
          })
     );
     return arrStatus;
@@ -15,7 +21,15 @@ function extraiLinks(arrLinks){
     return arrLinks.map((objetoLink) => Object.values(objetoLink).join());
 }
 
-
+function manejaErros(erro){
+    // console.log(chalk.red('Algo deu errado'));
+    //cause - causa do erro
+    if(erro.cause.code==="ENOTFOUND"){
+        return 'Link não encontrado'
+    } else{
+        return 'Ocorreu um erro'
+    }
+}
 
 
 //criar uma função listaValidada
@@ -25,7 +39,12 @@ export default async function listaValidada(listaDeLinks){
     const links = extraiLinks(listaDeLinks);
     const status = await checaStatus(links);
     //console.log(status);
-    return status;
+    return listaDeLinks.map((objeto,indice) => ({
+        //distriuir informação, esparramar - ...
+        ...objeto,
+        status:status[indice]
+    }));
+    //percorrer a lista, desmembra as informações,daí junta a variável status com o status propriamente dito (índice)
 }
 
 
